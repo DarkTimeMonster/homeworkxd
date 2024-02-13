b11fa53523878fea162686c130dcec0709be39ec
Prime <Prime@DESKTOP-CHDOR6K>
2024-02-13 20:17:34 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int i;
+            /*
+                        //1) Выполнить инициализацию одномерного массива с использованием конструктора без указания значений элементов, заполнить массив с клавиатуры и вывести на экран.
+                            int[]mas = new int[10];
+
+                            //заполнение ОМ с клавиатуры
+                                for (i = 0; i < mas.Length; i++)
+                                {   
+                                Console.Write($"omas[{i}] = ");
+                                mas[i] = Convert.ToInt32(Console.ReadLine());
+
+                                }
+                                for (i = 0;i < mas.Length; i++)
+                                {
+                                Console.WriteLine($"omas[{i}] = {mas[i]}");
+                                }
+
+                        /*
+                        //2) Инициализировать одномерный массив с использованием конструктора , без указания размерности массива и без клавиатуры.
+                        int[] mas = {-3,5,75,23,12,23};
+                        for (int i = 0; i < mas.Length; i++)
+                        {
+                        Console.WriteLine($"omas[{i}] = {mas[i]}");
+                        }
+                        */
+
+           
+        }
+    }
+}
